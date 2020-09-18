be97396ffe8ea09eaf8bb22e7853838a1b053c5a
VonArzu <Von.Arzu@rutgers.edu>
2020-09-18 15:55:30 -0400

    backend and authentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  post '/auth/login', to: 'authentication#login'
-  get '/auth/verify', to: 'authentication#verify'
-  resources :users
-
   resources :brands, only: :index
   resources :kicks
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
   resources :users, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
