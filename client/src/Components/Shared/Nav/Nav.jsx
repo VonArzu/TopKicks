7624f5ccd489da4b8462f36cfb328e14f240999f
VonArzu <Von.Arzu@rutgers.edu>
2020-09-21 05:16:11 -0400

    updates

---
+++ client/src/Components/Shared/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Nav.css";
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="navb">
+        <div className="linksb">
+          <NavLink className="linkb" to="/">
+            HOME
+          </NavLink>
+          <NavLink className="linkb" to="/AllKicks">
+            KICKS
+          </NavLink>
+          <NavLink className="linkb" to="/CreateKick">
+            ADDKICK
+          </NavLink>
+          <NavLink className="linkb" to="/Brands">
+            BRANDS
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
